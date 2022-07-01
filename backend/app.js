66e7e9c60ad3a3c0ba85ae17762e7f4753e35aff
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-01 14:08:46 -0500

    app sever

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const  mongoose = require('mongoose')
+const app = express()
+const port = 5555
+
+app.use(express.json())
+app.use(expres.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/exampledb')
+
+const exampleController = require('')
+app.use('/example', exampleController)
+
+app.listen(port, () => console.log(`server running on ${port}`))
